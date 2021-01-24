fa2969d681073d5362cadafc51b824f07bbd66d8
jbeard <player69>
2021-01-23 22:00:02 -0700

    added new search feature for languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 
 var formSubmitHandler = function(event) {
@@ -21,6 +22,20 @@ var formSubmitHandler = function(event) {
     alert("Please enter a GitHub username");
   }
 };
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "http://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response){
+    if(response.ok){
+      response.json().then(function(data){
+        displayRepos(data.items, language);
+      })
+    } else{
+      alert("Error: " + response.statusText);
+    }
+  })
+}
 
 var getUserRepos = function(user) {
   // format the github api url
@@ -91,5 +106,17 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language");
+  
+  if(language){
+    getFeaturedRepos(language);
+  }
+
+  // clear old content
+  repoContainerEl.textContent = "";
+}
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
